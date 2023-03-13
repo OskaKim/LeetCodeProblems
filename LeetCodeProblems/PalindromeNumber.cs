a8ef733cbd33fb6a2fd5c995be8acd373f150109
OskaKim <o2kim0225@naver.com>
2023-03-13 23:18:59 +0900

    PalindromeNumber

---
+++ LeetCodeProblems/PalindromeNumber.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace PalindromeNumber {
+
+    public class Solution {
+        public bool IsPalindrome(int x) {
+            if (x < 0) return false;
+            if (x < 10) return true;
+
+            List<int> nums = new List<int>(1);
+            while(x > 0) {
+                nums.Add(x % 10);
+                x /= 10;
+            }
+
+            for(int i = 0; i < nums.Count / 2; ++i) {
+                if(nums[i] != nums[nums.Count - 1 - i]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    [TestClass]
+    public class PalindromeNumber {
+        readonly Solution solution = new Solution();
+
+        [TestMethod]
+        public void Test1() {
+            Assert.IsTrue(solution.IsPalindrome(0));
+            Assert.IsTrue(solution.IsPalindrome(1));
+            Assert.IsTrue(solution.IsPalindrome(11));
+            Assert.IsTrue(solution.IsPalindrome(121));
+            Assert.IsFalse(solution.IsPalindrome(-121));
+            Assert.IsFalse(solution.IsPalindrome(10));
+        }
+    }
+}
